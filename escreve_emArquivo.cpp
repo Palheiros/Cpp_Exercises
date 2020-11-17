00317a68853ea8892c13d02f1139c0e8d5105a88
Ricardo Palheiros <47124346+Palheiros@users.noreply.github.com>
2020-11-17 11:41:21 -0300

    Update escreve_emArquivo.cpp

--- escreve_emArquivo.cpp
+++ escreve_emArquivo.cpp
@@ -1,22 +1,21 @@
-// programa que cria um arquivo externo e escreve algo nele toda vez que È executado
-// Ao executar o programa no terminal nada ir· acontecer, porÈm sera gerado um arquivo 'entrada.txt' com o texto inserido
+// programa que cria um arquivo externo e escreve algo nele toda vez que √© executado
+// Ao executar o programa no terminal nada ir√° acontecer, por√©m sera gerado um arquivo 'entrada.txt' com o texto inserido
 
 #include <stdio.h>
-#include <string.h>
 #include <fstream>
 
 using namespace std;
 
 int main (){
 
-    // cria o arquivo de saÌda
+    // cria o arquivo de sa√≠da
     ofstream arquivoDeSaida;
 
-    // abre o arquivo ou o cria caso n„o exista
+    // abre o arquivo ou o cria caso n√£o exista
     arquivoDeSaida.open("entrada.txt", std::ios_base::app);
 
     // escreve no arquivo toda vez que o programa for executado
-    arquivoDeSaida << "VocÍ executou o programa e algo foi escrito.\n";
+    arquivoDeSaida << "Voc√™ executou o programa e algo foi escrito.\n";
 
     // fecha o arquivo
     arquivoDeSaida.close();
